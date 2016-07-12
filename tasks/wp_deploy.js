stephenharris/grunt-wp-deploy

commit cd7234b0be1c6581ed22a70af6bdc20bfc8856eb

Split out adding & committing assets. Fixes bug introduced in df20d73e5f09b452004d577f908f5736835882f5

tasks/wp_deploy.js
@@ -237,13 +237,23 @@ module.exports = function(grunt) {
 		});
 	};
 
+	var addAssets = function( ctxt, callback ) {
+		var cmd = "svn status | " + awk + " '/^[?]/{print $2}' | xargs --no-run-if-empty svn add;"; //Add new files
+		cmd += "svn status | " + awk + " '/^[!]/{print $2}' | xargs --no-run-if-empty svn delete;"; //Remove missing files
+ 
+		exec( cmd,{ cwd: ctxt.svnpath+"/assets" }, function(error, stdout, stderr) {
+			if (error !== null) {
+				grunt.fail.warn( 'Failed to add assets: ' + error );
+			}
+			callback( null, ctxt );
+		} );
+	};
+
 	var commitAssets = function( ctxt, callback ) {
 		var assetCommitMsg = "Committing assets for " + ctxt.new_version;
 		grunt.log.writeln( assetCommitMsg + "\n" );
 
-		var cmd = "svn status | grep -v '^.[ \t]*\\..*' | grep '^?' | " + awk + " '{print $2}' | xargs svn add;"; //Add new files
-		cmd += "svn status | grep -v '^.[ \t]*\\..*' | grep '^!' | " + awk + " '{print $2}' | xargs svn delete;"; //Remove missing files
-		cmd += 'svn commit ' + ctxt.force_interactive + ' --username="'+ctxt.svnuser+'" -m "'+assetCommitMsg+'"';
+		var cmd = 'svn commit ' + ctxt.force_interactive + ' --username="'+ctxt.svnuser+'" -m "'+assetCommitMsg+'"';
 
 		exec( cmd,{ cwd: ctxt.svnpath+"/assets" }, function(error, stdout, stderr) {
 			if (error !== null) {

commit e577d76f6b57078e66858d826205f2d4a0baa50b

Don't use --no-run-if-empty as this is failing tests on OSX (invalid option). Ref #24. Reverts df20d73e5f09b452004d577f908f5736835882f5 and fixes bug introduced in cd7234b0be1c6581ed22a70af6bdc20bfc8856eb

tasks/wp_deploy.js
@@ -238,8 +238,9 @@ module.exports = function(grunt) {
 	};
 
 	var addAssets = function( ctxt, callback ) {
-		var cmd = "svn status | " + awk + " '/^[?]/{print $2}' | xargs --no-run-if-empty svn add;"; //Add new files
-		cmd += "svn status | " + awk + " '/^[!]/{print $2}' | xargs --no-run-if-empty svn delete;"; //Remove missing files
+	
+		var cmd = "svn status | grep -v '^.[ \t]*\\..*' | grep '^?' | " + awk + " '{print $2}' | xargs svn add;";
+		cmd += "svn status | grep -v '^.[ \t]*\\..*' | grep '^!' | " + awk + " '{print $2}' | xargs svn delete;";
  
 		exec( cmd,{ cwd: ctxt.svnpath+"/assets" }, function(error, stdout, stderr) {
 			if (error !== null) {