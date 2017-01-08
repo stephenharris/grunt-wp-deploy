ec0330d985e4f3964057dd0cab8619e3d23bb589
Stephen Edgar <stephen@netweb.com.au>
2017-01-08 16:32:02 +1100

    Remove .php extension

--- tasks/wp_deploy.js
+++ tasks/wp_deploy.js
@@ -106,7 +106,7 @@ module.exports = function(grunt) {
 			//Check versions
 			if( options.deploy_tag && projectVersionCompare( pluginVersion[1],  readmeVersion[1] )  !== 0 ){
 				grunt.log.warn( "Readme.txt version: " + readmeVersion[1] );
-				grunt.log.warn( plugin_file+".php version: " + pluginVersion[1] );
+				grunt.log.warn( plugin_file+" version: " + pluginVersion[1] );
 				grunt.fail.warn( 'Versions do not match:');
 			}
 
